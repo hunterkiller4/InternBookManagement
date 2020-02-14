773fecfaa187387ab667f7b4437d6e67ff0811d1
hoanghoang124 <52622951+hoanghoang124@users.noreply.github.com>
2020-02-14 09:31:58 +0700

    * style login, register form 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from "./register/register.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { BookComponent } from './book/book.component';
-import { BookStartComponent } from './book/book-start/book-start.component';
-import { BookDetailComponent } from './book/book-detail/book-detail.component';
-import { BookEditComponent } from './book/book-edit/book-edit.component';
-import { LoginComponent } from './login/login.component';
+import { BookComponent } from "./book/book.component";
+import { BookStartComponent } from "./book/book-start/book-start.component";
+import { BookDetailComponent } from "./book/book-detail/book-detail.component";
+import { BookEditComponent } from "./book/book-edit/book-edit.component";
+import { LoginComponent } from "./login/login.component";
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'book', component: BookComponent, children: [
-    { path: '', component: BookStartComponent },
-    { path: 'new', component: BookEditComponent },
-    { path: ':id', component: BookDetailComponent },
-    { path: ':id/edit', component: BookEditComponent },
-  ] },
-  { path: 'login', component: LoginComponent }
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  {
+    path: "book",
+    component: BookComponent,
+    children: [
+      { path: "", component: BookStartComponent },
+      { path: "new", component: BookEditComponent },
+      { path: ":id", component: BookDetailComponent },
+      { path: ":id/edit", component: BookEditComponent }
+    ]
+  },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  registered = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+      tc: ["", Validators.required]
+    });
   }
 
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.registered = true;
+    if (this.registerForm.valid) {
+      this.router.navigate(["/book"], { relativeTo: this.route });
+    }
+  }
 }
